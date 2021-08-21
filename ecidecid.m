5ea752fc1210881cd14d27f7a6396905d5148a31
Cameron Katri <me@cameronkatri.com>
2021-08-21 19:15:16 -0400

    ecidecid: Tabs not spaces

--- ecidecid.m
+++ ecidecid.m
@@ -3,9 +3,9 @@
 CFNumberRef MGCopyAnswer(CFStringRef string);
 
 int main() {
-    uint64_t ecid;
-    CFNumberRef ecidRef = MGCopyAnswer(CFSTR("UniqueChipID"));
-    CFNumberGetValue(ecidRef, kCFNumberSInt64Type, &ecid);
-    printf("0x%llX\n", ecid);
+	uint64_t ecid;
+	CFNumberRef ecidRef = MGCopyAnswer(CFSTR("UniqueChipID"));
+	CFNumberGetValue(ecidRef, kCFNumberSInt64Type, &ecid);
+	printf("0x%llX\n", ecid);
 	return 0;
 }
