b652eefb9eb89d3e7cde6e5666c9e4a43c2dcb80
Cameron Katri <me@cameronkatri.com>
2021-10-18 08:14:49 -0400

    Add uishoot(1) (#18)
    Co-authored-by: sunflsks <22717474+sunflsks@users.noreply.github.com>

--- Makefile
+++ Makefile
@@ -6,8 +6,8 @@ STRIP   ?= strip
 LDID    ?= ldid
 INSTALL ?= install
 
-ALL := gssc ldrestart sbdidlaunch sbreload uicache uiopen deviceinfo uialert
-MAN := gssc.1 ldrestart.1 sbdidlaunch.1 sbreload.1 uicache.1 uiopen.1 deviceinfo.1 uialert.1
+ALL := gssc ldrestart sbdidlaunch sbreload uicache uiopen deviceinfo uialert uishoot
+MAN := gssc.1 ldrestart.1 sbdidlaunch.1 sbreload.1 uicache.1 uiopen.1 deviceinfo.1 uialert.1 uishoot.1
 ALLMAC := gssc deviceinfo uialert
 MANMAC := gssc.1 deviceinfo.1 uialert.1
 
@@ -20,6 +20,7 @@ sign: $(ALL)
 	$(LDID) -Ssbreload.plist sbreload
 	$(LDID) -Suicache.plist uicache
 	$(LDID) -Suiopen.plist uiopen
+	$(LDID) -Suishoot.plist uishoot
 
 all: sign
 
@@ -43,6 +44,9 @@ uicache: uicache.m uicache.plist
 
 uiopen: uiopen.m ent.plist
 	$(CC) -fobjc-arc -O3 $(CFLAGS) uiopen.m -o uiopen $(LDFLAGS) -framework Foundation -framework MobileCoreServices
+
+uishoot: uishoot.m strtonum.c uishoot.plist
+	$(CC) -fobjc-arc -O3 $(CFLAGS) uishoot.m strtonum.c -o uishoot $(LDFLAGS) -framework ImageIO -framework Photos -framework UIKit
 
 deviceinfo: deviceinfo.c ecidecid.m uiduid.m serial.m locale.m cfversion.c
 	$(CC) -fobjc-arc -O3 $(CFLAGS) $^ -o $@ $(LDFLAGS) -framework CoreFoundation -lMobileGestalt
---
+++ uishoot.m
@@ -0,0 +1,125 @@
+#include <err.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <stdio.h>
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <Photos/Photos.h>
+
+OBJC_EXTERN UIImage *_UICreateScreenUIImage(void);
+
+void usage(uint8_t ret) {
+	fprintf(stderr, "uishoot [-cp] [-d number] [-f [png | jpeg | heic]] [-o file]\n");
+	exit(ret);
+}
+
+int main(int argc, char** argv) {
+	bool copyToClipboard = false;
+	bool saveToPhotos = false;
+	long long int delay = 0;
+	NSString* filePath = nil;
+	NSString* imageFormat = @"png";
+	int c;
+	const char *errstr;
+	__block int ret = 0;
+
+	static struct option longopts[] = {
+		{ "clipboard",	no_argument,	NULL,		'b' },
+		{ "photos",	no_argument,	NULL,		'p' },
+		{ "delay",	required_argument,	NULL,		'd' },
+		{ "format",	required_argument,	NULL,		'f' },
+		{ "output",	required_argument,	NULL,		'o' },
+		{ NULL,		0,			NULL, 		0 }
+	};
+
+	while ((c = getopt_long(argc, argv, "cpd:f:o:", longopts, NULL)) != -1) {
+		switch (c) {
+			case 'c':
+				copyToClipboard = true;
+				break;
+			case 'o':
+				filePath = [NSString stringWithUTF8String:optarg];
+				break;
+			case 'p':
+				saveToPhotos = true;
+				break;
+			case 'd':
+				delay = strtonum(optarg, 0, INT_MAX, &errstr);
+				if (errstr != NULL)
+					errx(1, "the timout is %s: %s", errstr, optarg);
+				break;
+			case 'f':
+				imageFormat = [NSString stringWithUTF8String:optarg];
+				if (imageFormat && ![imageFormat isEqualToString:@"png"] && ![imageFormat isEqualToString:@"jpeg"] && ![imageFormat isEqualToString:@"heic"]) {
+					fprintf(stderr, "Invalid image format '%s'\n", imageFormat.UTF8String);
+					usage(2);
+				}
+				break;
+			case '?':
+				usage(1);
+				break;
+			default:
+				usage(1);
+		}
+	}
+
+	if (optind <= 1)
+		usage(1);
+
+	if (delay) sleep(delay);
+
+	UIImage* screenShot = _UICreateScreenUIImage();
+	if (!screenShot) {
+		fprintf(stderr, "Could not capture screenshot!\n");
+		return 2;
+	}
+
+	if (copyToClipboard) { 
+		FILE* old_stderr = stderr;
+		stderr = fopen("/dev/null", "w");
+
+		[UIPasteboard generalPasteboard].image = screenShot;
+
+		fclose(stderr);
+		stderr = old_stderr;
+	}
+
+	if (filePath) {
+		NSError* error;
+		NSString* imageUTI = [NSString stringWithFormat:@"public.%@", imageFormat];
+
+		NSMutableData* imageData = [[NSMutableData alloc] init];
+		CGImageDestinationRef destinationRef = CGImageDestinationCreateWithData((CFMutableDataRef)imageData, (CFStringRef)imageUTI, 1, NULL);
+		CGImageDestinationAddImage(destinationRef, screenShot.CGImage, NULL);
+		if (!CGImageDestinationFinalize(destinationRef)) {
+			fprintf(stderr, "Could not get image data to write to file!\n");
+			ret = 3;
+		}
+
+		CFRelease(destinationRef);
+
+		if (ret != 3 && ![imageData writeToFile:filePath options:NSDataWritingAtomic error:&error]) {
+			fprintf(stderr, "Could not write image to %s: %s\n", filePath.UTF8String, error.localizedDescription.UTF8String);
+			ret = 3;
+		}
+	}
+
+	if (saveToPhotos) {
+		dispatch_semaphore_t sema = dispatch_semaphore_create(0);
+
+		[[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
+			[PHAssetChangeRequest creationRequestForAssetFromImage:screenShot];
+		} completionHandler:^(BOOL success, NSError* error) {
+			if (!success) {
+				fprintf(stderr, "Could not save screenshot to Photos: %s\n", error.localizedDescription.UTF8String);
+				ret = 3;
+			}
+
+			dispatch_semaphore_signal(sema);
+		}];
+
+		dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
+	}
+
+	return ret;
+}
